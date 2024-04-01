4a7f09654b39cfe5a3c9a53c359220311ac0dac8
jordan fezeu <101807569+Jordanfez@users.noreply.github.com>
2024-04-01 19:22:55 +0100

    dernier step et ajout du dossier image

---
+++ app/src/Components/contentStepWrapperDonneMetheo/index.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Container, FormControl, FormLabel, Input, Select, Stack } from '@chakra-ui/react';
+
+const theme = {
+    colors: {
+      gray: {
+        100: '#817876',
+      },
+    },
+  }
+
+export const DonneMetheo = () => {
+    return (
+
+        <Container>
+            <Box >
+                <FormControl mb={4} width={'100%'} mt={25}>
+                    <FormLabel >Sélectionnez une Zone geographique :</FormLabel>
+                    <Select>
+                        <option value="option1">Littoral</option>
+                        <option value="option2">Centre</option>
+                        <option value="option3">Extreme Nord</option>
+                        <option value="option3">Extreme sud</option>
+                        <option value="option3">Sud</option>
+                        <option value="option3">EST</option>
+                    </Select>
+                </FormControl>
+
+                <FormControl mb={4} width={'100%'}>
+                    <FormLabel>Date :</FormLabel>
+                    <Input type="date" />
+                </FormControl>
+
+                <Stack direction='row' spacing={2} align='center'>
+                    <Button mt={4} colorScheme='gray' width={'100%'} borderRadius={'50px'} size='md'>
+                        Precedent
+                    </Button>
+
+                    <Button mt={4} colorScheme='teal' width={'100%'} borderRadius={'50px'} size='md'>
+                        Calculer
+                    </Button>
+                </Stack>
+            </Box>
+        </Container>
+    )
+}
