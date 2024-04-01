bb5070646dcea14595841dd7f3dbf23ea8652d90
tiemeni <tiemanirocket@gmail.com>
2024-04-01 14:59:49 +0100

    put in place the side bar menu

---
+++ app/src/Helper/data/index.ts
@@ -0,0 +1,7 @@
+export const data = [
+    { label: "Donnees d'entree", icon: '' },
+    { label: 'Modeles predefinis', icon: '' },
+    { label: 'Resultats de sortie', icon: '' },
+    { label: 'Aide', icon: '' },
+    { label: 'A propos', icon: '' },
+  ]
